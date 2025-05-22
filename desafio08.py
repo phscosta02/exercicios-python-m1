a6387a489967eebbdc40de9e0b8f803568d6d886
pedro <phscosta02@gmail.com>
2025-05-22 20:31:58 -0300

    Desafio 08 Mundo 1, Curso em Video.

---
+++ desafio08.py
@@ -0,0 +1,6 @@
+mt=float(input('sua altura: '))
+mm=mt*1000
+cm=mt*100
+print('sua altura é de {}'.format(mt))
+print('convertido em milimetros {:.0f}'.format(mm))
+print('convertido em centimetros {:.0f}'.format(cm))
